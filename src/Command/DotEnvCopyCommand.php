d851794fab44da4f0fc8b6ddcee64e5acdd845d2
ncou <nicolas_courade@yahoo.fr>
2020-11-22 07:57:49 +0100

    update

--- src/Command/DotEnvCopyCommand.php
+++ src/Command/DotEnvCopyCommand.php
@@ -19,12 +19,15 @@ final class DotEnvCopyCommand extends AbstractCommand
     {
         $this
             ->setDescription('Copy the dotenv file.')
+            ->addOption('destination', 'd', InputOption::VALUE_REQUIRED, 'Destination path for the .env file')
             ->addOption('force', 'f', InputOption::VALUE_NONE, 'Overwrite any existing files');
     }
 
-    protected function perform(Filesystem $filesystem, Directories $directories): int
+    protected function perform(Filesystem $filesystem): int
     {
-        $copied = $this->copyFile($filesystem, __DIR__ . '/../../resources/.env.sample', directory('@root/.env'));
+        $filepath = $this->option('destination');
+
+        $copied = $this->copyFile($filesystem, __DIR__ . '/../../resources/.env.sample', $filepath);
 
         if ($copied) {
             $this->success('Dotenv file ".env" has been copied.');
